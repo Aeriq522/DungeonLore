bb14529517e15f6291025e644717e04c4920b77f
Erik Sanchez <eriksanchez@Mac.localdomain>
2025-06-13 14:31:37 -0700

    fixed params issue in id params

--- app/sessions/[id]/page.tsx
+++ app/sessions/[id]/page.tsx
@@ -1,6 +1,12 @@
 import SessionDetails from '../../../components/SessionDetails';
 
-export default async function Page({ params }) {
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function Page({ params }: PageProps) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/sessions/${params.id}`, {
     cache: 'no-store',
   });
